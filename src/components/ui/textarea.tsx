e8cad5375ab5e90da56698ca172503b2be53212a
riz4d <rizadvillan@gmail.com>
2024-11-08 20:41:19 +0530

    Refactor Textarea component: Update import and classNames

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,13 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
-
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+const Textarea = React.forwardRef<HTMLTextAreaElement, React.TextareaHTMLAttributes<HTMLTextAreaElement>>(
   ({ className, ...props }, ref) => {
     return (
       <textarea
